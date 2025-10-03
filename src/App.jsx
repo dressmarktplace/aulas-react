c872d9387d2f6cb95c8f31bf0be1058d5bdcc019
MichelleSilva <dressmarktplace@gmail.com>
2025-10-02 21:44:51 -0300

    Implementado grid responsivo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 const App = () => {
   return (
-    <main className="min-h-dvh grid place-items-center bg-slate-50">
+    <main className="min-h-dvh place-items-center bg-slate-50">
       <h1 className="text-3xl font-bold text-slate-800">
         Hello Tailwind + React!
       </h1>
+
+      <div className="container mx-auto p-6 grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        <div className="rounded-2xl border p-4 bg-white shadow-sm">
+          Card A
+        </div>
+        <div className="rounded-2xl border p-4 bg-white shadow-sm">
+          Card B
+        </div>
+        <div className="rounded-2xl border p-4 bg-white shadow-sm">
+          Card C
+        </div>
+      </div>
     </main>
   );
 }
